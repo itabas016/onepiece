a8b2862489972bac841a1561e2cdaa96cdab0705
arthor_cui <arthor.cui@gmail.com>
2014-08-26 17:50:39 +0800

    add core registry mode factory test and core security various sign method test
    next step add internal or protect assembly sign

---
+++ src/OnePiece.Framework.Tests/Compile/RegistryModeFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿using OnePiece.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnePiece.Framework.Tests.Compile
+{
+    public class RegistryModeFactoryTests
+    {
+        [Fact]
+        public void GetCurrentMode_should_return_correct_mode()
+        {
+            RegistryModeFactory.ClearConditions();
+            var mode = RegistryModeFactory.GetCurrentMode();
+
+            Console.WriteLine(mode);
+
+            if (ProjectConfigHelper.IsInDebugMode())
+            {
+                Assert.Equal(mode, RegistryMode.Debug);
+            }
+
+            if (ProjectConfigHelper.IsInReleaseMode())
+            {
+                Assert.Equal(mode, RegistryMode.Release);
+            }
+
+            if (ProjectConfigHelper.IsInLiveMode())
+            {
+                Assert.Equal(mode, RegistryMode.Live);
+            }
+        }
+
+        [Fact]
+        public void GetCurrentMode_can_be_injected_outside_if_condition_is_single()
+        {
+            RegistryModeFactory.ClearConditions();
+            RegistryModeFactory.AddCondition(() => RegistryMode.Release);
+
+            var mode = RegistryModeFactory.GetCurrentMode();
+
+            Console.WriteLine(mode);
+            Assert.Equal(RegistryMode.Release, mode);
+        }
+
+        [Fact]
+        public void GetCurrentMode_can_be_injected_outside_if_condition_are_multiple()
+        {
+            RegistryModeFactory.ClearConditions();
+            RegistryModeFactory.AddCondition(() => RegistryMode.Release);
+            RegistryModeFactory.AddCondition(() => RegistryMode.Live);
+
+            var mode = RegistryModeFactory.GetCurrentMode();
+
+            Console.WriteLine(mode);
+            Assert.Equal(RegistryMode.Release | RegistryMode.Live, mode);
+        }
+    }
+}
