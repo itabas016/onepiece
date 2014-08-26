80fb305ddfc7057a80eb2787b4a86350e6f7215e
arthor_cui <arthor.cui@gmail.com>
2014-08-26 17:02:40 +0800

    add core extension clone method test and type extension test
    this clone is deep clone, is differ object protect memberwise shallow copy  method

---
+++ src/OnePiece.Framework.Tests/Core/Extensions/ObjectExtensionsTest.cs
@@ -0,0 +1,68 @@
+﻿using OnePiece.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnePiece.Framework.Tests.Core.Extensions
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void object_clone_test()
+        {
+            var person = new Person();
+            person.Name = "Bob";
+            person.Age = 12;
+            person.Marker = new Marker(1111);
+
+            var instance = person.ShallowCopy();
+            Show(person);
+            Show(instance);
+
+            Assert.Equal(person.Marker.Number, instance.Marker.Number);
+
+            person.Name = "Jack";
+            person.Age = 22;
+            person.Marker.Number = 2222;
+
+            var instance2 = person.Clone<Person>();
+            Show(person);
+            Show(instance2);
+
+            Assert.Equal(false, instance2.Equals(person));
+        }
+
+        public void Show(Person person)
+        {
+            Console.WriteLine(string.Format("Name: {0}, Age: {1}", person.Name, person.Age));
+            Console.WriteLine(string.Format("Marker Number: {0}", person.Marker.Number));
+        }
+
+        [Serializable]
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Marker Marker { get; set; }
+
+            public Person ShallowCopy()
+            {
+                return (Person)this.MemberwiseClone();
+            }
+        }
+
+        [Serializable]
+        public class Marker
+        {
+            public int Number { get; set; }
+
+            public Marker(int number)
+            {
+                this.Number = number;
+            }
+        }
+    }
+}
---
+++ src/OnePiece.Framework.Tests/Core/Extensions/TypeExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿using OnePiece.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnePiece.Framework.Tests.Core.Extensions
+{
+    public class TypeExtensionsTest
+    {
+        [Fact]
+        public void IsEnumerableType_test()
+        {
+            Assert.False((default(Type)).IsEnumerableType());
+
+            Assert.True(typeof(TestList).IsEnumerableType());
+            Assert.True(typeof(TestList).IsEnumerableType());
+
+            Assert.False(typeof(TestNormal).IsEnumerableType());
+            Assert.False(typeof(TestNormal).IsEnumerableType());
+        }
+
+        [Fact]
+        public void IsSerializableEnumerableType_test()
+        {
+            Assert.False((default(Type)).IsSerializableEnumerableType());
+
+            Assert.True(typeof(PersonList).IsSerializableEnumerableType());
+            Assert.True(typeof(PersonList).IsSerializableEnumerableType());
+
+            Assert.False(typeof(TestNormal).IsSerializableEnumerableType());
+            Assert.False(typeof(TestNormal).IsSerializableEnumerableType());
+        }
+
+        public class TestList : List<int>
+        { }
+
+        public class TestNormal { }
+
+        [Serializable]
+        public class Person
+        {
+            public int Age { get; set; }
+        }
+
+        public class PersonList : List<Person> { }
+    }
+}
