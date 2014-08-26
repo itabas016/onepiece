e1aef009fff037af79507db29e0a2268c8998664
arthor_cui <arthor.cui@gmail.com>
2014-08-26 15:48:10 +0800

    add predicate extension method test about logic calculate
    mean is or,and...

--- src/OnePiece.Framework.Core/Extensions/PredicateBuilder.cs
+++ src/OnePiece.Framework.Core/Extensions/PredicateBuilder.cs
@@ -20,8 +20,7 @@ namespace OnePiece.Framework.Core
                   (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
         }
 
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
-                                                       Expression<Func<T, bool>> expr2)
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
         {
             var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>
---
+++ src/OnePiece.Framework.Tests/Core/Extensions/PredicateBuilderTest.cs
@@ -0,0 +1,46 @@
+﻿using OnePiece.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnePiece.Framework.Tests.Core.Extensions
+{
+    public class PredicateBuilderTest
+    {
+        List<int> Repo = new List<int>();
+        public PredicateBuilderTest()
+        {
+            Enumerable.Range(0, 10).ToList().ForEach(x => Repo.Add(x));
+        }
+
+        [Fact]
+        public void test_and()
+        {
+            var predicate = PredicateBuilder.True<int>();
+
+            predicate = predicate.And<int>(i => i < 10 && i > 2);
+            predicate = predicate.And<int>(i => i > 5 && i < 9);
+
+            var ret = Repo.Where(predicate.Compile()).ToList();
+
+            Assert.Equal(3, ret.Count);
+        }
+
+
+        [Fact]
+        public void test_or()
+        {
+            var predicate = PredicateBuilder.False<int>();
+
+            predicate = predicate.Or<int>(i => i > 8 && i < 10);
+            predicate = predicate.Or<int>(i => i > 3 && i < 5);
+
+            var ret = Repo.Where(predicate.Compile()).ToList();
+
+            Assert.Equal(2, ret.Count);
+        }
+    }
+}
