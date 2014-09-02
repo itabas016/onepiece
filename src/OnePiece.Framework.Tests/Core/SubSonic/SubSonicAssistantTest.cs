ca7e60510eeb12bab3d233dec1bc79ee42703d8d
arthor_cui <arthor.cui@gmail.com>
2014-09-02 19:22:13 +0800

    add core subsonic assistant test contains index and comment sql script generate

---
+++ src/OnePiece.Framework.Tests/Core/SubSonic/SubSonicAssistantTest.cs
@@ -0,0 +1,29 @@
+﻿using OnePiece.Framework.SubSonic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnePiece.Framework.Tests.Core.SubSonic
+{
+    public class SubSonicAssistantTest
+    {
+        const string TestAssemblyName = "OnePiece.Framework.Tests";
+
+        [Fact]
+        public void index_generate_test()
+        {
+            var sql = IndexAssistant.Generate(TestAssemblyName);
+            Console.WriteLine(sql);
+        }
+
+        [Fact]
+        public void comment_generate_test()
+        {
+            var sql = CommentAssistant.Generate(TestAssemblyName);
+            Console.WriteLine(sql);
+        }
+    }
+}
---
+++ src/OnePiece.Framework.Tests/Core/SubSonic/SubSonicModelTest.cs
@@ -0,0 +1,25 @@
+﻿using OnePiece.Framework.Core.Data;
+using OnePiece.Framework.SubSonic;
+using SubSonic.SqlGeneration.Schema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePiece.Framework.Tests.Core.SubSonic
+{
+    [Serializable]
+    [SubSonicDatabase("testdb")]
+    [SubSonicTableNameOverride("test")]
+    public class SubSonicModelTest : EntityBase
+    {
+        [SubSonicStringLength(64)]
+        public string Name { get; set; }
+
+        [SubSonicIndex]
+        [Display(Name = "用户ID"), SqlColumn]
+        public int UserId { get; set; }
+    }
+}
